cca0f8f1cc3193baebafdb49350c76f4ad1c1432
Bobb Voigt <smyl@lyms.org>
2011-11-14 04:51:21 -0600

    added accountant (Accountant-3.11.40200)

---
+++ Accountant/localization.lua
@@ -0,0 +1,122 @@
+-- Header
+ACCLOC_TITLE		=	"Accountant";
+ACCLOC_TOT_IN		=	"Revenue";
+ACCLOC_TOT_OUT		=	"Expenditures";
+ACCLOC_NET			=	"Net Profit / Loss";
+ACCLOC_NETLOSS		=	"Net Loss";
+ACCLOC_NETPROF		=	"Net Profit";
+ACCLOC_SOURCE		=	"Source";
+ACCLOC_IN			=	"Revenue";
+ACCLOC_OUT			=	"Expenditures";
+ACCLOC_WEEKSTART	=	"Week Start";
+ACCLOC_SUM			=	"Sum Total";
+ACCLOC_CHARCAP		= "Character"
+ACCLOC_CHAR			= "character"
+ACCLOC_MONEY		=	"Money";
+ACCLOC_UPDATED		=	"Updated";
+ACCLOC_ALLIANCE		=  "Alliance"
+ACCLOC_HORDE		=  "Horde"
+
+-- Section Labels
+ACCLOC_LOOT			=	"Loot";
+ACCLOC_QUEST		=	"Quest Rewards";
+ACCLOC_MERCH		=	"Merchants";
+ACCLOC_TRADE		=	"Trade Window";
+ACCLOC_MAIL			=	"Mail";
+ACCLOC_TRAIN		=	"Training Costs";
+ACCLOC_TAXI			=	"Taxi Fares";
+ACCLOC_OTHER		=	"Unknown";
+ACCLOC_REPAIR		=	"Repair Costs";
+ACCLOC_AUC			=	"Auction House";
+ACCLOC_SYS			=	"Other System";
+
+-- Buttons
+ACCLOC_RESET		=	"Clear Data";
+ACCLOC_EXIT			=	"Exit";
+
+-- Tabs
+ACCLOC_SESS			=	"Session";
+ACCLOC_DAY			=	"Day";
+ACCLOC_WEEK			=	"Week";
+ACCLOC_TOTAL		=	"Total";
+ACCLOC_CHARS		=	"All Chars";
+
+-- Options
+ACCLOC_STARTWEEK	=	"Start of Week";
+ACCLOC_WD_SUN		=	"Sunday";
+ACCLOC_WD_MON		=	"Monday";
+ACCLOC_WD_TUE		=	"Tuesday";
+ACCLOC_WD_WED		=	"Wednesday";
+ACCLOC_WD_THU		=	"Thursday";
+ACCLOC_WD_FRI		=	"Friday";
+ACCLOC_WD_SAT		=	"Saturday";
+ACCLOC_DONE			=	"complete";
+
+-- Misc
+ACCLOC_RESET_CONF	=	"Are you sure you want to reset the";
+ACCLOC_NEWPROFILE	=	"New "..ACCLOC_TITLE.." profile created for";
+ACCLOC_LOADPROFILE	=	"Loaded "..ACCLOC_TITLE.." profile for";
+ACCLOC_LOADED		=	"Loaded";
+ACCLOC_RCLICK		= "Right-Click"
+ACCLOC_LCLICK		= "Left-Click"
+ACCLOC_REMOVE		= "Remove"
+ACCLOC_MERGE		= "Merge"
+ACCLOC_MERGETO		= " --> "
+ACCLOC_TOGGLE		= "Toggle"
+ACCLOC_MINIMAP		= "Minimap"
+ACCLOC_BUTTON		= "Button"
+
+-- Mail strings
+ACCLOC_AUCTION_SUCC     = "Auction successful:"
+ACCLOC_AUCTION_OUTBID   = "Outbid"
+
+-- Key Bindings headers
+BINDING_NAME_ACCOUNTANTTOG	=	ACCLOC_TOGGLE.." "..ACCLOC_TITLE;
+
+-- Config strings
+ACCLOC_MINIMAP_BUT		=	ACCLOC_MINIMAP.." "..ACCLOC_BUTTON;
+ACCLOC_CONF_DESC		=	ACCLOC_TITLE.." allows you to track where your gold is going.";
+ACCLOC_CONF_GEN_DESC	=	"Configure "..ACCLOC_MINIMAP.." "..ACCLOC_BUTTON
+ACCLOC_TOG_MINIMAP		= "Display "..ACCLOC_MINIMAP.." "..ACCLOC_BUTTON
+ACCLOC_TOG_MINIMAP_DESC	= "Display the "..ACCLOC_MINIMAP.." "..ACCLOC_BUTTON
+ACCLOC_POS_MINIMAP		= ACCLOC_MINIMAP.." "..ACCLOC_BUTTON.." Position"
+ACCLOC_POS_MINIMAP_DESC	= "Position the "..ACCLOC_MINIMAP.." "..ACCLOC_BUTTON.." on the "..ACCLOC_MINIMAP..""
+ACCLOC_TRANSPARANCY		= "Transparancy"
+ACCLOC_TRANSP			= "Change the "..ACCLOC_TRANSPARANCY.." of the "..ACCLOC_TITLE.." window"
+ACCLOC_TRANSP_DESC		= "Change "..ACCLOC_TRANSPARANCY
+ACCLOC_STARTWEEK_DESC	=	"Change the day of the week that "..ACCLOC_TITLE.." uses. This resets the "..ACCLOC_STARTWEEK.." totals.";
+ACCLOC_WEEKDAY			=	"Week Day";
+ACCLOC_WEEKDAY_DESC		=	"Select the day of the week.";
+ACCLOC_WEEKDAY_DONE		=	"Change to "..ACCLOC_WEEKDAY;
+ACCLOC_TOON				=	ACCLOC_CHARCAP
+ACCLOC_TOON_MGMT		=	ACCLOC_CHARCAP.." Management";
+ACCLOC_TOON_MGMT_2		=	"It is recommended that you save your "..ACCLOC_TITLE.." data before proceeding - just in case. The data is found in the "..ACCLOC_TITLE..".lua file in the SavedVariables folder.";
+ACCLOC_TOON_MGMT_DESC	=	"Manage the "..ACCLOC_TITLE.." data of your "..ACCLOC_CHAR.."(s).";
+ACCLOC_TOON_DELETE		=	ACCLOC_REMOVE;
+ACCLOC_TOON_REMOVE		=	ACCLOC_TOON.." "..ACCLOC_TOON_DELETE;
+ACCLOC_TOON_DELETE_DESC	=	ACCLOC_REMOVE..ACCLOC_TITLE.." data for the selected "..ACCLOC_CHAR;
+ACCLOC_TOON_DELETE_SELF	=	"You can not remove yourself!"
+ACCLOC_TOON_DELETE_DONE	=	ACCLOC_REMOVE.." of "
+ACCLOC_TOON_REMOVE_DESC	=	ACCLOC_REMOVE..ACCLOC_TITLE.." data for this "..ACCLOC_CHAR
+ACCLOC_TOON_REMOVE_NOTES=	"This will remove the "..ACCLOC_TITLE.." data for the selected "..ACCLOC_CHAR..". You must exit the game or log into another "..ACCLOC_CHAR.." to save the changes."
+ACCLOC_TOON_IGNORE		=	"Ignore";
+ACCLOC_TOON_IGNORE_DESC	=	"Ignore "..ACCLOC_TITLE.." data for this "..ACCLOC_CHAR..". Do not collect data.";
+ACCLOC_TOON_MERGE		=	"Merge";
+ACCLOC_TOON_MERGE_T		=	ACCLOC_TOON.." "..ACCLOC_TOON_MERGE;
+ACCLOC_TOON_FROM		=	"From:"
+ACCLOC_TOON_TO			=	"To:"
+ACCLOC_TO				=	"to"
+ACCLOC_TOON_MERGE_ERR1	=	"Can not merge a "..ACCLOC_CHAR.." to itself!!";
+ACCLOC_TOON_FROM_DESC	=	"Merge "..ACCLOC_TITLE.." data from this "..ACCLOC_CHAR;
+ACCLOC_TOON_TO_DESC		=	"Merge "..ACCLOC_TITLE.." data to this "..ACCLOC_CHAR;
+ACCLOC_TOON_MERGE_NOTES1=	""
+ACCLOC_TOON_MERGE_NOTES2=	"NOTE: If you merge "..ACCLOC_TITLE.." data 'to' the "..ACCLOC_CHAR.." you are logged into session data will be lost. You should also log into the "..ACCLOC_CHAR.." you want to merge 'from'. Otherwise week data may not be correct (depending on when you last logged into that "..ACCLOC_CHAR..")."
+ACCLOC_RESET_DESC		= 	"Clear the "..ACCLOC_TITLE.." data for all characters on this realm only: "
+							..GREEN_FONT_COLOR_CODE..GetRealmName()
+ACCLOC_RESET_DESC_SHORT	= 	"Clear data for "..GREEN_FONT_COLOR_CODE..GetRealmName()
+
+if ( locale == "deDE" ) then
+	-- Mail strings
+	ACCLOC_AUCTION_SUCC     = "Auktion erfolgreich:"
+	ACCLOC_AUCTION_OUTBID   = "überboten"
+end
