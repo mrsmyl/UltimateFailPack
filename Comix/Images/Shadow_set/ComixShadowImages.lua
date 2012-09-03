9bee5bdb2e6f6f0ef2f8590738d6c17a4a06f04b
Bobb Voigt <smyl@MADCOW>
2012-09-03 00:55:02 -0500

    Updated Comix to 1.86

--- Comix/Images/Shadow_set/ComixShadowImages.lua
+++ Comix/Images/Shadow_set/ComixShadowImages.lua
@@ -1,8 +1,8 @@
 -- Shadow Grafix --
 ComixShadowImages = {};
-ComixShadowImages[1] = "shadow1.blp";
-ComixShadowImages[2] = "shadow2.blp";
-ComixShadowImages[3] = "shadow3.blp";
-ComixShadowImages[4] = "shadow4.blp";
-ComixShadowImages[5] = "shadow5.blp";
-ComixShadowImages[6] = "shadow6.blp";
+ComixShadowImages[1] = "Images\\Shadow_set\\shadow1.blp";
+ComixShadowImages[2] = "Images\\Shadow_set\\shadow2.blp";
+ComixShadowImages[3] = "Images\\Shadow_set\\shadow3.blp";
+ComixShadowImages[4] = "Images\\Shadow_set\\shadow4.blp";
+ComixShadowImages[5] = "Images\\Shadow_set\\shadow5.blp";
+ComixShadowImages[6] = "Images\\Shadow_set\\shadow6.blp";
