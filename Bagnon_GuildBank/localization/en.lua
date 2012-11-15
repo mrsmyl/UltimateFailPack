4a298b91edea911b8fb95bf46cf35fd86a663cfe
Mistah SMyl <smyl@ultimatefail.net>
2012-11-15 16:47:31 -0600

    Updated Bagnon to 5.0.9

--- Bagnon_GuildBank/localization/en.lua
+++ Bagnon_GuildBank/localization/en.lua
@@ -1,11 +1,10 @@
 --[[
-	Bagnon Guild Bank Localization Information: English Language
-		This file must be present to have partial translations
+	Bagnon Guild Bank Localization: English
 --]]
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-GuildBank', 'enUS', true)
 
-L.Title = [[%s's Guild Bank]]
+L.Title = "%s's Guild Bank"
 L.Log1 = 'Transaction Log'
 L.Log3 = 'Tab Information'
 L.TipFunds = 'Guild Funds'
